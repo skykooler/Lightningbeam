adeb066a946fc898e6ff4e5e699756662230036c
Skyler Lehmkuhl <skycooler@gmail.com>
2012-01-14 17:07:38 -0500

    Added setup script

---
+++ setup.py
@@ -0,0 +1,53 @@
+"""
+py2app/py2exe build script for Lightningbeam.
+
+Will automatically ensure that all build prerequisites are available
+via ez_setup
+
+Usage (Mac OS X):
+    python setup.py py2app
+
+Usage (Windows):
+    python setup.py py2exe
+"""
+import ez_setup
+ez_setup.use_setuptools()
+
+import sys
+from setuptools import setup
+
+
+mainscript = 'lightningbeam.py'
+
+if sys.platform == 'darwin':
+    extra_options = dict(
+        setup_requires=['py2app'],
+        app=[mainscript],
+        # Cross-platform applications generally expect sys.argv to
+        # be used for opening files.
+        options=dict(py2app=dict(argv_emulation=True,
+        plist=dict(
+            #CFBundleDocumentTypes= ,
+            CFBundleIdentifyer='org.lightningbeam.lightningbeam',
+            #LSPrefersPPC=True,
+        ),
+        resources=["media","gpl.txt"],
+        iconfile="Lightningbeam.icns"
+        )),
+    )
+elif sys.platform == 'win32':
+    extra_options = dict(
+        setup_requires=['py2exe'],
+        app=[mainscript],
+    )
+else:
+     extra_options = dict(
+         # Normally unix-like platforms will use "setup.py install"
+         # and install the main script as such
+         scripts=[mainscript],
+     )
+
+setup(
+    name="Lightningbeam",
+    **extra_options
+)
