f1c350aad84b9444aea6540b6c9bce018ca25f9c
Skyler Lehmkuhl <skycooler@gmail.com>
2013-01-05 19:52:09 -0500

    Changed TextInput to CodeInput for syntax highlighting

--- kt.py
+++ kt.py
@@ -2,6 +2,7 @@ from random import random
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
+from kivy.uix.codeinput import CodeInput
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.button import Button
 from kivy.graphics import Color, Ellipse, Line
